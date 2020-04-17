86c310e69b5d3b46ef4792108fe223926ee7343d
Muhammad Kashaf <kashafahmed72@gmail.com>
2020-04-17 23:44:51 +0500

    'api-issue'

--- Screens/SignUp/index.js
+++ Screens/SignUp/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Image, KeyboardAvoidingView } from 'react-native';
-import { Icon, Item, Input, Picker, Button } from 'native-base';
+import { StyleSheet, Text, View, ImageBackground, Image, KeyboardAvoidingView, Alert } from 'react-native';
+import { Icon, Item, Input, Picker, Button, loader } from 'native-base';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Divider } from 'react-native-elements';
 import firebase from '../../Component/Config/Firebase';
+import axios from 'axios';
 
 export default class SignUp extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-            userType: '',
+            firstName: 'user',
+            lastName: 'rider',
+            email: 'ride1@gmail.com',
+            password: '12345678',
+            firstNameErr: '',
+            lastNameErr: '',
+            emailErr: '',
+            passwordErr: '',
+            userType: 2,
             currentUser: null
         }
     }
@@ -24,42 +29,116 @@ export default class SignUp extends React.Component {
         })
     }
 
+    // signUp = () => {
+    //     const { email, password, firstName, lastName, userType } = this.state
+
+    //     // console.log(email, password)
+    //     firebase.auth().createUserWithEmailAndPassword(email, password)
+    //         .then((user) => {
+
+    //             user.user.updateProfile({
+    //                 displayName: `${firstName} ${lastName}`,
+    //                 photoURL: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    //             }).then(function () {
+    //                 const userObj = {
+    //                     name: `${firstName} ${lastName}`,
+    //                     email: user.user.email,
+    //                     userType: userType,
+    //                     profilePic: user.user.photoURL
+    //                 }
+    //                 firebase.database().ref(`/users/${user.user.uid}/`).push(userObj)
+    //                 // userType === "Rider" && this.props.navigation.navigate('OrdersScreen')
+    //             }, function (err) {
+    //                 console.log(err)
+    //             })
+
+    //             this.setState({
+    //                 firstName: '',
+    //                 lastName: '',
+    //                 email: '',
+    //                 password: '',
+    //                 userType: '', currentUser: user.user
+    //             })
+    //         })
+    //         .then((user) => console.log('user', user))
+    //         .catch((err) => alert(err))
+    // }
+
+
+
     signUp = () => {
-        const { email, password, firstName, lastName, userType } = this.state
+        this.setState({ loader: true })
 
-        // console.log(email, password)
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then((user) => {
+        const { email, password, firstName, lastName } = this.state
 
-                user.user.updateProfile({
-                    displayName: `${firstName} ${lastName}`,
-                    photoURL: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                }).then(function () {
-                    const userObj = {
-                        name: `${firstName} ${lastName}`,
-                        email: user.user.email,
-                        userType: userType,
-                        profilePic: user.user.photoURL
-                    }
-                    firebase.database().ref(`/users/${user.user.uid}/`).push(userObj)
-                    // userType === "Rider" && this.props.navigation.navigate('OrdersScreen')
-                }, function (err) {
-                    console.log(err)
-                })
+        console.log("SIGN UP jksdajkfajkshjghj")
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (email && password && firstName && lastName) {
+            if (reg.test(email) === false) {
+                this.setState({ loader: false })
+                Alert.alert("Alert", "Please Enter Valid Email Address")
+            } else {
+                let obj = {
+                    "roll_id": this.state.userType,
+                    "email": email,
+                    "password": password,
+                    "fname": firstName,
+                    "lname": lastName,
 
-                this.setState({
-                    firstName: '',
-                    lastName: '',
-                    email: '',
-                    password: '',
-                    userType: '', currentUser: user.user
-                })
-            })
-            .then((user) => console.log('user', user))
-            .catch((err) => alert(err))
+                }
+                const formData = new FormData();
+
+
+                formData.append("roll_id", this.state.userType),
+                    formData.append("email", email.toLowerCase()),
+                    formData.append("password", password),
+                    formData.append("fname", firstName),
+                    formData.append("lname", lastName),
+
+
+
+                    console.log("********************************", obj);
+
+
+
+                axios.post("https://144.91.70.164/Turabi/signup.php", formData)
+                    .then(response => {
+
+                        console.log(response);
+
+
+                        if (response.status) {
+                            Alert.alert("Alert", "Signup successful")
+                            this.setState({ loader: true })
+                            this.props.navigation.navigate("Login")
+                        }
+                        else {
+                            this.setState({ loader: false })
+                            Alert.alert("Error", response.message)
+
+                        }
+                        console.log("Alert", response, response.status)
+
+                    })
+                    .catch(err => console.log("*-*-*-*", err))
+
+
+            }
+        } if (!email) {
+            this.setState({ emailErr: true, loader: false })
+        } if (!firstName || firstName.length < 5) {
+            this.setState({ firstName: true, loader: false })
+        } if (!lastName) {
+            this.setState({ lastNameErr: true, loader: false })
+        }
+        if (password.length < 6) {
+            this.setState({ passwordErr: true, loader: false })
+        }
     }
 
     render() {
+        const { email, password, firstName, lastName, firstNameErr, lastNameErr, emailErr, passwordErr } = this.state
+
         return (
             <View style={styles.container}>
                 <ImageBackground source={require('../../assets/BACKground-01.jpg')} style={{ width: '100%', height: '100%', opacity: 0.9 }} >
@@ -83,38 +162,47 @@ export default class SignUp extends React.Component {
                                     onValueChange={(userType) => this.setState({ userType })}
                                 >
                                     <Picker.Item label="Select User Type" value="key0" />
-                                    <Picker.Item label="Rider" value="Rider" />
-                                    <Picker.Item label="Customer" value="Customer" />
+                                    <Picker.Item label="Rider" value="2" />
+                                    <Picker.Item label="Customer" value="3" />
                                 </Picker>
                             </Item>
 
                             <View style={{ display: 'flex', flexDirection: 'row' }} >
                                 <View>
-                                    <Text style={styles.text} >First Name</Text>
+                                    <Text style={styles.text}>First Name</Text>
                                     <Item regular style={{}} >
                                         <Input placeholder='First Name' value={this.state.firstName} onChangeText={(firstName) => this.setState({ firstName })} />
                                     </Item>
+                                    {/* {firstNameErr && <Text style={{ color: 'red', fontSize: 12, alignSelf: 'flex-end' }} >FirstName is required</Text>} */}
+
                                 </View>
+
                                 <View style={{ marginLeft: '10%' }} >
-                                    <Text style={styles.text} >Last Name</Text>
+                                    <Text style={styles.text}>Last Name</Text>
                                     <Item regular style={{}}>
                                         <Input placeholder='Last Name' value={this.state.lastName} onChangeText={(lastName) => this.setState({ lastName })} />
                                     </Item>
+                                    {/* {lastNameErr && <Text style={{ color: 'red', fontSize: 12, alignSelf: 'flex-end' }} >LastName is required</Text>} */}
+
                                 </View>
                             </View>
 
                             <View>
-                                <Text style={styles.text} >E-mail</Text>
+                                <Text style={styles.text}>E-mail</Text>
                                 <Item regular style={{ width: '80%', marginTop: '2%' }} >
                                     <Input placeholder='example. hnhsolution...' value={this.state.email} onChangeText={(email) => this.setState({ email })} />
                                 </Item>
+                                {/* {emailErr && <Text style={{ color: 'red', fontSize: 12, alignSelf: 'flex-end', marginRight: '10%' }} >Email address is required</Text>} */}
+
                             </View>
 
                             <View style={{ marginTop: '5%' }} >
-                                <Text style={styles.text} >Password</Text>
+                                <Text style={styles.text}>Password</Text>
                                 <Item regular style={{ width: '80%', marginTop: '2%' }}>
                                     <Input onChangeText={(password) => this.setState({ password })} placeholder='*********' value={this.state.password} secureTextEntry />
                                 </Item>
+                                {/* {passwordErr && <Text style={{ color: 'red', fontSize: 12, alignSelf: 'flex-end', marginRight: '10%' }} >Password length must be greater than 6 digits</Text>} */}
+
                             </View>
 
                         </View>
